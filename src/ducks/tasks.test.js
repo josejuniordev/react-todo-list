2e4b5de437c8b8d34aa001308f91dafedc6c6da5
José Junior <junioradorart@gmail.com>
2019-03-22 17:35:28 -0300

    feat: add some unity tests

---
+++ src/ducks/tasks.test.js
@@ -0,0 +1,80 @@
+import taskReducer, {
+  FETCH_TASKS, fetchTasksAction,
+  FETCH_TASKS_FAILED, fetchTasksFailedAction,
+  FETCH_TASKS_SUCCESS, fetchTasksSuccessAction,
+  DELETE_TASK, deleteTaskAction,
+  UPDATE_TASK, updateTaskAction,
+  UPDATE_TASKS_ON_LIST, updateTasksOnListAction,
+  TOGGLE_TASK_STATUS, toggleTaskStatusAction,
+  INSERT_NEW_TASK, insertNewTaskAction
+} from './tasks';
+
+import TasksMockedData from '../mock/TaskMock';
+
+describe('(Ducks) Tasks', () => {
+
+  describe('Action Creators', () => {
+
+    it('fetchTasksAction returns an action of type FETCH_TASKS', () => {
+      expect(fetchTasksAction().type)
+        .toBe(FETCH_TASKS);
+    });
+
+    it('fetchTasksFailedAction returns an action of type FETCH_TASKS_FAILED', () => {
+      expect(fetchTasksFailedAction().type)
+        .toBe(FETCH_TASKS_FAILED);
+    });
+
+    it('fetchTasksSuccessAction returns an action of type FETCH_TASKS_SUCCESS', () => {
+      expect(fetchTasksSuccessAction().type)
+        .toBe(FETCH_TASKS_SUCCESS);
+    });
+
+    it('deleteTaskAction returns an action of type DELETE_TASK', () => {
+      expect(deleteTaskAction().type)
+        .toBe(DELETE_TASK);
+    });
+
+    it('updateTaskAction returns an action of type UPDATE_TASK', () => {
+      expect(updateTaskAction().type)
+        .toBe(UPDATE_TASK);
+    });
+
+    it('updateTasksOnListAction returns an action of type UPDATE_TASKS_ON_LIST', () => {
+      expect(updateTasksOnListAction().type)
+        .toBe(UPDATE_TASKS_ON_LIST);
+    });
+
+    it('toggleTaskStatusAction returns an action of type TOGGLE_TASK_STATUS', () => {
+      expect(toggleTaskStatusAction().type)
+        .toBe(TOGGLE_TASK_STATUS);
+    });
+
+    it('insertNewTaskAction returns an action of type INSERT_NEW_TASK', () => {
+      expect(insertNewTaskAction().type)
+        .toBe(INSERT_NEW_TASK);
+    });
+
+  });
+
+  describe('Reducer', () => {
+
+    it('returns state even if the action is unknown', () => {
+      const state = {
+        data: []
+      };
+      expect(taskReducer(state, { type: 'UNKNOWN_ACTION' }))
+        .toBe(state);
+    });
+
+    it('FETCH_TASKS_SUCCESS sets parsed tasks in data property', () => {
+      const state = {
+        data: []
+      };
+      expect(
+        taskReducer(state, fetchTasksSuccessAction(TasksMockedData))
+          .data
+      ).toMatchObject(TasksMockedData)
+    });
+  });
+});
---
+++ src/mock/TaskMock.js
@@ -0,0 +1,75 @@
+import TasksUtils from '../utility/TasksUtils';
+
+export const TasksData = [
+  {
+    "id": "1",
+    "createdAt": "2019-03-21T19:46:35.970Z",
+    "description": "The EXE application is down, input the 1080p card so we can calculate the PCI firewall!",
+    "time": "2019-07-21T18:03:23.917Z",
+    "rememberTime": 74,
+    "durationTime": 4,
+    "status": "pending",
+    "tags": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "id": "2",
+    "createdAt": "2019-03-21T11:00:36.158Z",
+    "description": "We need to override the auxiliary JSON program!",
+    "time": "2019-12-14T19:54:00.730Z",
+    "rememberTime": 83,
+    "durationTime": 96,
+    "status": "pending",
+    "tags": [
+      1
+    ]
+  },
+  {
+    "id": "3",
+    "createdAt": "2019-03-21T20:54:18.349Z",
+    "description": "You can't calculate the hard drive without synthesizing the online ADP application!",
+    "time": "2019-06-08T12:59:36.213Z",
+    "rememberTime": 16,
+    "durationTime": 74,
+    "status": "done",
+    "tags": [
+      1,
+      2
+    ]
+  },
+  {
+    "id": "4",
+    "createdAt": "2019-03-21T08:57:34.424Z",
+    "description": "I'll hack the redundant SSL panel, that should capacitor the THX firewall!",
+    "time": "2020-02-24T10:47:42.021Z",
+    "rememberTime": 69,
+    "durationTime": 81,
+    "status": "pending",
+    "tags": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "id": "5",
+    "createdAt": "2019-03-21T15:52:13.761Z",
+    "description": "Use the online GB protocol, then you can reboot the virtual driver!",
+    "time": "2019-04-01T00:33:25.444Z",
+    "rememberTime": 66,
+    "durationTime": 84,
+    "status": "pending",
+    "tags": [
+      1
+    ]
+  }
+];
+
+const tasksMockedData = TasksData.map(task => {
+  return TasksUtils.taskFactory(task);
+});
+
+export default tasksMockedData;
