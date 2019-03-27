f68c8bfe6bc7c3d1c891ecbdc7451aeafc301491
José Junior <junioradorart@gmail.com>
2019-03-27 09:32:56 -0300

    feat: add some tests to FilterMethods.

--- src/shared/tasksFilterMethods.test.js
+++ src/shared/tasksFilterMethods.test.js
@@ -1,10 +1,40 @@
 import tasksFilterMethods from './tasksFilterMethods';
 import tasksMockedData from '../mock/TasksMock';
+import { taskStatus } from './tasksConstantes';
 
 describe('Tasks filter methods', () => {
 
+  it('All method returns the same list', async () => {
+    const filteredData = await tasksFilterMethods.all(tasksMockedData);
+
+    expect(filteredData).toBe(tasksMockedData);
+  });
+
   it('Done method returns the tasks that has DONE status', async () => {
     const filteredData = await tasksFilterMethods.done(tasksMockedData);
+    let hasOnlyDone = true;
+
+    filteredData.forEach(task => {
+      if (task.status !== taskStatus.DONE) {
+        hasOnlyDone = false;
+      }
+    });
+
     expect(filteredData.length).toBe(1);
+    expect(hasOnlyDone).toBe(true);
+  });
+
+  it('Pending method returns the tasks that has PENDING status', async () => {
+    const filteredData = await tasksFilterMethods.pending(tasksMockedData);
+    let hasOnlyPending = true;
+
+    filteredData.forEach(task => {
+      if (task.status !== taskStatus.PENDING) {
+        hasOnlyPending = false;
+      }
+    });
+
+    expect(filteredData.length).toBe(5);
+    expect(hasOnlyPending).toBe(true);
   });
 });
