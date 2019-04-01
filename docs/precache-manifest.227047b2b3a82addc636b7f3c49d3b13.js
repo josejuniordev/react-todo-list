8773f9c2917f8f1ad53d5d7c3d9d53c63c37dbe1
José Junior <junioradorart@gmail.com>
2019-04-01 12:30:10 -0300

    feat: deploy to gh pages

---
+++ docs/precache-manifest.227047b2b3a82addc636b7f3c49d3b13.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8c5dcce2d4fddd18977",
+    "url": "/react-todo-list/static/js/0.3bcdc962.chunk.js"
+  },
+  {
+    "revision": "0fd99ab7d4a43e3b7730",
+    "url": "/react-todo-list/static/css/main.ec997647.chunk.css"
+  },
+  {
+    "revision": "0fd99ab7d4a43e3b7730",
+    "url": "/react-todo-list/static/js/main.82007f87.chunk.js"
+  },
+  {
+    "revision": "e36586373d7dee118d83",
+    "url": "/react-todo-list/static/js/runtime~main.02c7ebe3.js"
+  },
+  {
+    "revision": "d755f2b379fcc00daab7",
+    "url": "/react-todo-list/static/css/3.12f18311.chunk.css"
+  },
+  {
+    "revision": "d755f2b379fcc00daab7",
+    "url": "/react-todo-list/static/js/3.bf17937e.chunk.js"
+  },
+  {
+    "revision": "116da6a12eaf15455fcb",
+    "url": "/react-todo-list/static/js/4.2bd87a4b.chunk.js"
+  },
+  {
+    "revision": "ad2870e6e3c87d430d91",
+    "url": "/react-todo-list/static/js/5.c173d6b8.chunk.js"
+  },
+  {
+    "revision": "c916acf4dc18a70b2da8",
+    "url": "/react-todo-list/static/js/6.09e03ccf.chunk.js"
+  },
+  {
+    "revision": "f23bc8ae38287ca54d67",
+    "url": "/react-todo-list/static/js/7.717a77f6.chunk.js"
+  },
+  {
+    "revision": "0f457cb5ebe6612c3e54",
+    "url": "/react-todo-list/static/js/8.bfb57915.chunk.js"
+  },
+  {
+    "revision": "30142b0ce1791574c331",
+    "url": "/react-todo-list/static/js/9.23756336.chunk.js"
+  },
+  {
+    "revision": "f85f823e62c57115d9ccfb83606aeb7b",
+    "url": "/react-todo-list/index.html"
+  }
+];
